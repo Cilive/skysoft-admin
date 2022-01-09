c7005a49225cdab3be6123bf000109fa4d8b8053
mohammedshafi3 <shafimohammedpopzz@gmail.com>
2022-01-09 19:01:50 +0530

    pump employee api

--- src/app/components/owner/pump-employee/pump-employee.model.ts
+++ src/app/components/owner/pump-employee/pump-employee.model.ts
@@ -1,10 +1,30 @@
 export interface Employee {
   phone: string;
-  password?: string;
+  branches: number;
+  iqama_no: string;
+  username: string;
+  password: string;
   email: string;
   name: string;
   id?: number;
-  account?: {
-    email: string;
-  };
+  user?: User;
+}
+export interface User {
+  id: number;
+  is_superuser: boolean;
+  username: string;
+  email: string;
+  is_company: boolean;
+  is_active: boolean;
+  is_employee: boolean;
+  is_branch_user: boolean;
+  is_staff: boolean;
+  is_admin: boolean;
+  is_super_admin: boolean;
+  created_at: Date;
+  updated_at: Date;
+  date_joined: Date;
+  last_login: Date;
+  groups: any[];
+  user_permissions: any[];
 }
