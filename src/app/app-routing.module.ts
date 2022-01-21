d2ef5bcd1188261e876ffc56897418306307c0aa
docsploit <robertdevasia@outlook.com>
2022-01-22 01:10:10 +0530

    branch manager and ui module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
@@ -8,10 +9,15 @@ const routes: Routes = [
       (await import('./components/authentication/authentication.module'))
         .AuthenticationModule,
   },
+  {
+    path: 'ui',
+    loadChildren: async () =>
+      (await import('./components/ui/ui.module')).UiModule,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), FormsModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/components/owner/users/users.component.ts
+++ src/app/components/owner/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Role } from 'src/app/model/shared';
+import { StoreService } from 'src/app/services/store/store.service';
 
 @Component({
   selector: 'app-users',
@@ -33,7 +35,28 @@ export class UsersComponent implements OnInit {
       title: 'Owners',
     },
   ];
-  constructor() {}
+  constructor() {
+    const role = new StoreService().retrieve('role');
+    if (Role.maneger === Number(role)) {
+      this.tileList = [
+        {
+          icon: 'bx bxs-group',
+          routerLink: '/branch/branch-employee',
+          title: 'Employees',
+        },
+        {
+          icon: 'bx bxs-user-voice',
+          routerLink: '/branch/supplier_Profile',
+          title: 'Suppliers',
+        },
+        {
+          icon: 'bx bxs-factory',
+          routerLink: '/branch/customer_Profile',
+          title: 'Customers',
+        },
+      ];
+    }
+  }
 
   ngOnInit(): void {}
   domier(string: string, index) {
