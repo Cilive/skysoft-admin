2f17a295084d37607ad534904d2a7615af64dee7
mohammednoorulameen <noorulameen8606>
2022-01-23 21:14:11 +0530

    noor api fuel management jan23sun

--- src/app/services/bank-account-master/bank-account-master.service.ts
+++ src/app/services/bank-account-master/bank-account-master.service.ts
@@ -46,12 +46,7 @@ export class BankAccountMasterService {
     );
   }
 }
- get_bank_account(id) {
-    return this.http.get<ApiResponse<Branch[]>>(
-      environment.domain + '/clients/c1.localhost/private/bank/' + id + '/'
-    );
-
-    
-  }
-
-
+//  get_bank_account(id) {
+//     return this.http.get<ApiResponse<Branch[]>>(
+//       environment.domain + '/clients/c1.localhost/private/bank/' + id + '/'
+//     );
