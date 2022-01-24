dbc4c2a9dc9c83ec7bf062548ebea9792721395e
mohammednoorulameen <noorulameen8606>
2022-01-24 14:59:12 +0530

    noor api supplier fuel management jan 23sun

---
+++ src/app/components/owner/vat-master/vat-master.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vat-master',
+  templateUrl: './vat-master.component.html',
+  styleUrls: ['./vat-master.component.scss']
+})
+export class VatMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
