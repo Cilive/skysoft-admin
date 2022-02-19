004ccb9a2cf7652293cd9507c1d65258eb48bb98
mohammednoorulameen <noorulameen8606>
2022-02-19 15:14:22 +0530

    bug fixing feb19sat

---
+++ src/app/components/branch/fuelstock/fuelstock.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fuelstock',
+  templateUrl: './fuelstock.component.html',
+  styleUrls: ['./fuelstock.component.scss'],
+})
+export class FuelstockComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
