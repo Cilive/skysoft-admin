e3652e43316b39b3eb6af331e1c5ae353e01ba8e
mohammednoorulameen <noorulameen8606>
2022-02-20 18:15:42 +0530

    noor branch report customer feb20sun

--- src/app/components/branch/components/reports/reports.component.ts
+++ src/app/components/branch/components/reports/reports.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  tileItems = [
+    // {
+    //   path: 'readings',
+    //   icon: 'bx bx-spreadsheet',
+    //   title: 'Meter Reading',
+    // },
+    {
+      path: 'customer_Balance',
+      icon: 'bx bx-spreadsheet',
+      title: 'Customer Balance',
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
+  domier(string: string, index) {
+    return (document.querySelectorAll('div.overlay')[index].innerHTML = string);
   }
-
 }
+
+interface Tile {
+  title: string;
+  routerLink: string;
+  icon: string;
+}
+type Tiles = Tile[];
