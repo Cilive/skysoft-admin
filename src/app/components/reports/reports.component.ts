40937c40e02026981316dcecd22898ff1668e6a3
muhsin kuttasseri <49359204+muhsinkuttasseri@users.noreply.github.com>
2022-02-25 10:36:29 +0530

    Removed unwanted lines of code

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -44,10 +44,7 @@ export class ReportsComponent implements OnInit, AfterViewInit {
           /[^a-zA-Z ]/g,
           ' '
         );
-        // document.title = capitalize(e.url.split('/').pop()).replace(
-        //   /[^a-zA-Z ]/g,
-        //   ' '
-        // );
+      
       }
     });
 
